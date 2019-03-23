7161ea5314d0c8c2d87c07a65468bf109a3fad88
Fred <fred1n@hotmail.com>
2019-03-23 08:46:32 -0500

    Code Cleanup

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@ const gameBands = ["Poison","Metallica","Duran Duran","Motely Crue","Gun N Roses
   "Def Leppard","Madonna","Prince","Queen","Bon Jovi"];
 
 const imageBands = ["poison.jpg","metallica.jpg","duranduran.jpg","motley-crue.jpg",
-"guns-n-roses.png","van-helan.jpg","defleppard.jpg","madonna.png","prince.jpg",
+"guns-n-roses.png","van-halen.jpg","defleppard.jpg","madonna.png","prince.jpg",
 "queen.png","bonjovi.jpg"]
 
 
@@ -14,13 +14,11 @@ var letterGuessed = [];       // Letter guessed
 var letterMatchedCtr = 0;     // Counter for letters that matched
 var guessesLeftCtr = 12;      // Total # of guesses left...initialized to 12
 var holdImageIndex = 0;       // Hold index of band selected
-// var gameOver = false;         // 
-// var winnerFlag = false;
-// var letterAlreadyFound = false;
 var bandImage;                // Variable to hold the band image location
 var wrongKeyPressed = "Games Accepts only letters!!"
 var exitMessage = "Game over!!"
 var winMessage = "Winner...Press Restart-Game button to start a new game!!"
+var loserMessage = "Maximum Number of 12 Guesses Met...game is resetting!"
 
 // Function
 // Main function that starts the game
@@ -30,20 +28,15 @@ function gameStart() {
   // Selects random word from array based on # of bands 
   randomWord = gameBands[Math.floor(Math.random() * gameBands.length)];
  
-  // console.log(randomWord);
-
   // Set letters to lowercase
   lcrrandomWord = randomWord.toLowerCase();
   // Removes spaces
   lcrrandomWord = lcrrandomWord.replace(/\s+/g, '');
 
   console.log(lcrrandomWord);
-  // console.log(lcrrandomWord.length);
 
   // hold the index of the band found to locate image based on band location
   holdImageIndex = gameBands.indexOf(randomWord);
-
-  // console.log(holdImageIndex);
 
   // Call function to determine underScore based on words length
   generateUnderScore();
@@ -54,7 +47,6 @@ function gameStart() {
  
   //Print guesses left to page
   document.getElementById("guesses-left").textContent = guessesLeftCtr;
-  // console.log("guessesLeftCtr "+guessesLeftCtr);
 }
 
 // Function
@@ -64,13 +56,6 @@ document.onkeyup = function(event) {
   // capture key pressed
   letterGuessed = event.key;
 
-  // console.log(winnerFlag);
-  // if (winnerFlag) {
-  //   alert("WINNER4444!")
-  //   winnerFlag = false;
-  //   resetGame();
-  // }
-
   // blank out label at bottom of page
   document.getElementById("loser-Label").textContent = " ";
 
@@ -78,9 +63,7 @@ document.onkeyup = function(event) {
   var key = event.keyCode;
   // valid letter codes
   if (key >= 65 && key <= 90)  {
-     // console.log("letterGuessed "+letterGuessed);
-    // console.log("randomWord.indexOf(letterGuessed) "+lcrrandomWord.indexOf(letterGuessed));
-    
+
     // Checks to see if letter pressed is in the random word
     if (lcrrandomWord.indexOf(letterGuessed) > -1){
       // Loops thru random word based on it's length
@@ -91,10 +74,8 @@ document.onkeyup = function(event) {
           underScore[i] = letterGuessed;
           // Update page with underscore and remove comma's
           document.getElementById("underScoreW").textContent = underScore.join(" ");
-
+          // Display to log
           console.log("underscore2 "+ underScore);
-          // guessesLeftCtr--;
-          // document.getElementById("guesses-left").textContent = guessesLeftCtr;
 
           // Call function to update counters and display # of guesses left
           updateWrongGuess();
@@ -103,7 +84,6 @@ document.onkeyup = function(event) {
           // Call Function to determine if is word has been guessed
           determineWinLose();
         }
-
       }
     } else {
       // Check to see if letter pressed has already been entered
@@ -111,7 +91,6 @@ document.onkeyup = function(event) {
         } else {
           // Push wrong letter guessed to array
           wrongLetter.push(letterGuessed);
-          // console.log("wrongLetter: " + wrongLetter);
           // Display wrong letter guessed to page
           document.getElementById("letters-guessed").textContent = wrongLetter;
           //Reset arrays
@@ -132,20 +111,20 @@ document.onkeyup = function(event) {
 function generateUnderScore() {
   // Loop thru random word and set the underscore based on the length
   for (var i = 0;i < lcrrandomWord.length; i++){
+    // Push underscore to array
     underScore.push("_");
   }
-  // Diplay on page
+  // Display on page
   document.getElementById("underScoreW").textContent = underScore.join(" ");
+  // Display word being guessed on console
   console.log("UnderScore "+ underScore);
-
 }
 
 // Function
-// Wrong letter was guess, decrement the # of possible guesses
+// Wrong letter was guessed, decrement the # of possible guesses
 //  display the count on page
 //======================================================= 
 function updateWrongGuess() {
-
   // Decrement letters left
   guessesLeftCtr--;
   // Display guesses left on page
@@ -162,80 +141,52 @@ function determineWinLose(){
   // alert("Check winner called")
   // Check letter guessed against random word length 
   if (letterMatchedCtr === lcrrandomWord.length) {
-    // console.log("winnner");
-
     // Update page with song from band on top
     document.getElementById("winner-Label").textContent = randomWord;
+    // Update message on bottom of page
     document.getElementById("loser-Label").textContent = winMessage;
+    // Set image to variable
     bandImage= "./assets/images/" + imageBands[holdImageIndex];
     console.log(bandImage);
+    // Update image on page 
     document.getElementById("band-image").setAttribute("src",bandImage);
-    // setImage();
-    // document.getElementById("band-image").innerHTML = "./assets/images/defleppard.jpg";
-    // winnerFlag=true;
-    // let i = 0;
-    // setTimeout(() => alert("You have won...press Ok to reset game!"), 100); // 100000000
-    // assume that the time to execute this function is >100ms
-    // for(let j = 0; j < 100000000; j++) {
-    //   i++;
-    // }
-    // resetGame();
+
   } // Check # of guesses left to see if they have any left
   else if(guessesLeftCtr === 0)
   {
-    // Update page that they've lost at bottom
-    // alert("lose");
-    // if (confirm("Do you want to continue?")) {
-    //   // Save it!
-    //   alert("yes");
-    // } else {
-    //   // Do nothing!
-    //   alert("no");
-    // }
-    
-  // var answer = confirm("Save data?")
-  // if (answer) {
-  //   //some code
-  //   alert("yes");
-  // }
-  // else {
-  //   //some 
-    // alert("no");
+    // Update page 
+    //  clear # of guesses
+    //  reset underscore on page
+    //  display update message to exit game
     document.getElementById("guesses-left").textContent = " ";
     document.getElementById("underScoreW").textContent = "____";
     document.getElementById("loser-Label").textContent = exitMessage;
     
-  
-    // wins = 0;
-    // losses = 0;
-    // clearArrays();
-    // resetGame();
-  } 
+  } // Check to see if total # of guess has been reached
   if(guessesLeftCtr === 0) {
-    let i = 0;
-    setTimeout(() => alert("Maximum Number of 12 Guesses Met...game is resetting!"), 100); // 100000000
-    // assume that the time to execute this function is >100ms
-    for(let j = 0; j < 100000000; j++) {
-      i++;
-    }
-
-        console.log("herewait");
-    // alert();
+    // display alert message on page
+    alert(loserMessage); 
+   
+    // Reset game
     resetGame();
   }
 }
 
+// Function - ResetGame
+//  Calls reload function to reset game
 function resetGame() {
   location.reload();
 }
 
+// Function - SetImage
+//  set image to variable
+//  prepare image to display on page
 function setImage() {
     
   bandImage= "./assets/images/" + imageBands[holdImageIndex];
-  console.log(bandImage);
   document.getElementById("band-image").setAttribute("src",bandImage);
 }
 
-
-
+// Function - GameStart
+//  Main function to start that starts the game
 gameStart();
